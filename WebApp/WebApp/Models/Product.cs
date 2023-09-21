6a256f0b02815080f0c3c12fe5a1100248742e65
Valeh <amirbekovaleh@gmail.com>
2023-09-22 01:15:20 +0400

    CRUD with SqlClient api

---
+++ WebApp/WebApp/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Brand { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public decimal Price { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
